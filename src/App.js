8c6dc9bb3b75c6ce5b8eac4b068ce8461e45adc8
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-30 20:35:15 +0530

    Adding input fields as well as state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
+import './App.css';
 
 class App extends Component{
     
   fetchWeather=async(query)=>{
    
-      const API_KEY="ea5f0bd6a1160a597e4bd4cca046ca66";
-    const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}&units=metric`)
+const API_KEY="ea5f0bd6a1160a597e4bd4cca046ca66";
+const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}&units=metric`)
 const data=await response.json();
 console.log(data)
 
@@ -14,8 +14,8 @@ console.log(data)
     
     render(){
         return(
-            <div>
-                Hello
+            <div className="main-container">
+              <input className="search" placeholder="search" value={} onChange={}/>
             </div>
         )
     }
