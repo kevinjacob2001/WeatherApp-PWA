d64224d525f1a6db72fefb99ae6958ecd87a26a1
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-30 20:32:49 +0530

    added the styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-const App=()=>{
-    return(
-        <h1>App</h1>
-    )
+class App extends Component{
+    
+  fetchWeather=async(query)=>{
+   
+      const API_KEY="ea5f0bd6a1160a597e4bd4cca046ca66";
+    const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}&units=metric`)
+const data=await response.json();
+console.log(data)
+
+  }
+    
+    render(){
+        return(
+            <div>
+                Hello
+            </div>
+        )
+    }
 }
 
 export default App;
