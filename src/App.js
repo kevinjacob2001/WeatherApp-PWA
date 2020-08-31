9d0c980557439b95362a4ae9aecc8be3cb88c171
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-31 13:57:07 +0530

    Added form and button

--- src/App.js
+++ src/App.js
@@ -13,18 +13,22 @@ class App extends Component{
 handleChange=(e)=>{this.setState({value:e.target.value})}
 
 handleSubmit=async(e)=>{
+  e.preventDefault();
 const API_KEY="ea5f0bd6a1160a597e4bd4cca046ca66";
 const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${API_KEY}&units=metric`)
 const data=await response.json();
-this.setState({info:data})
+this.setState({info:data.main})
 console.log(this.state.info)
 }
     
     render(){
         return(
             <div className="main-container">
-              <input className="search" placeholder="search" onChange={this.handleChange} value={this.state.value} />
-             <button onClick={this.handleSubmit}>CLICK</button>
+              <form onSubmit={this.handleSubmit}>
+              <input type="text" className="search" placeholder="search" onChange={this.handleChange} value={this.state.value} />
+             <button className="btn" onClick={this.handleSubmit}>Find weather!</button>
+             </form>
+           
             </div>
         )
     }
