bb0c506809629078e38631c0bac771dffdd58795
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-08-31 14:15:30 +0530

    Adding the card

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 class App extends Component{
   constructor(){
       super()
-      this.state={
-          value:"",
-          info:[]
-      }
+     
   }
+  state={
+    value:"",
+    info:[]
+}
 
 handleChange=(e)=>{this.setState({value:e.target.value})}
 
@@ -17,7 +18,7 @@ handleSubmit=async(e)=>{
 const API_KEY="ea5f0bd6a1160a597e4bd4cca046ca66";
 const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${API_KEY}&units=metric`)
 const data=await response.json();
-this.setState({info:data.main})
+this.setState({info:data})
 console.log(this.state.info)
 }
     
@@ -26,9 +27,27 @@ console.log(this.state.info)
             <div className="main-container">
               <form onSubmit={this.handleSubmit}>
               <input type="text" className="search" placeholder="search" onChange={this.handleChange} value={this.state.value} />
-             <button className="btn" onClick={this.handleSubmit}>Find weather!</button>
+            
              </form>
-           
+           {this.state.info.main&&(
+             <div className="city">
+<h2 className="city-name">
+           <span>{this.state.info.name}</span>
+           <sup>{this.state.info.sys.country}</sup>
+         
+            </h2>
+            <div className="city-temp">
+            {Math.round(this.state.info.main.temp)}
+            <sup>&deg;C</sup>
+            </div>
+
+            <div className="info">
+              <img className="city-info"/>
+            </div>
+
+             </div>
+           )}
+
             </div>
         )
     }
